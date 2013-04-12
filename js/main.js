6dfbfb37364d0a12c0d01030454ab5fce394f69f
Farhan Abrol <fabrol92@gmail.com>
2013-04-12 15:40:33 -0400

    adding tooltips

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function() {
+	$("#NormalShift,#RequestShift,#CoveredShift").each(
+		function() {
+			var openTip = new Opentip(this, {hideDelay:0.0});
+			openTip.setContent(this.id)
+		});
+});
