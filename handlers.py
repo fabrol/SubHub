edf56bfdfd06d99bf47c300e903b4c8ecdc60bb1
Farhan Abrol <fabrol92@gmail.com>
2013-04-27 01:31:47 -0400

    add handler for getting the shifts

--- handlers.py
+++ handlers.py
@@ -4,6 +4,7 @@ from google.appengine.ext import ndb
 import logging
 import os.path
 import webapp2
+import json
 
 from webapp2_extras import auth
 from webapp2_extras import sessions
@@ -15,9 +16,26 @@ from BaseHandlers import *
 
 '''Write all the custom handlers here'''
 
+def json_object_serializer(obj):
+  if hasattr(obj, 'isoformat'):
+    return obj.isoformat()
+  else:
+    raise TypeError(('Object of type {0} with value of {1} is ' 'not JSON serializable').format(type(obj), repr(obj)))
+
 class GetShiftsHandler(BaseHandler):
   @user_required
   def get(self):
     s = Shift.query()
-    params = {'shifts':s}
-    self.response.out.write(params)
+    # SEND BACK THE USER AS WELL. NOT THE OBJECT BUT INSTEAD THE ACTUAL VALUES OF THE PROPERTIES OF THE USER ASSOCIATED WITH EACH USER
+    shifts = []
+    for shift in s:
+      vals = {}
+      vals['datetime']=shift.datetime
+      vals['sub']=shift.sub
+      vals['duration']=shift.duration
+      vals['status']=shift.status
+      shifts.append(vals)
+    result = {'shifts': shifts}
+    response=json.dumps(result, default=json_object_serializer)
+    self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+    self.response.out.write(response)
