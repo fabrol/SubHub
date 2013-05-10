65ce15eee4f3180386517537b6adac3cf5b06267
Farhan Abrol <fabrol92@gmail.com>
2013-05-09 21:34:40 -0400

    fixed navbar and form headings

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	$('#navbar').scrollspy()
+          $('ul.nav li.active').removeClass('active');
+          var url = window.location;
+          $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+          $('ul.nav a').filter( function() { 
+            return this.href == url;
+          }).parent().addClass('active');
 });
